462def09c8dc1e2a3ac9a3a56fa7309f822355a3
Aman Kumar <amancollege04@gmail.com>
2025-04-30 05:44:10 +0530

    JobHunt project setup using Vite, Tailwind CSS and Shadcn UI

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+const App = () => {
+  return (
+    <div className='bg-gray-800 text-white w-full min-h-screen'>
+      <h1>JobHunt</h1>
+      <Button>Click Me!</Button>
+    </div>
+  )
+}
+
+export default App
