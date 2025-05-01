42bfef2e203c774a1a7319a029a7e09da0f558f5
Aman Kumar <amancollege04@gmail.com>
2025-05-01 16:10:33 +0530

    Add company data, theme toggle component, dropdown menu implementation, and Supabase client setup
    - Created a new JSON file for company data with names, paths, and IDs.
    - Implemented a ModeToggle component for theme switching using a dropdown menu.
    - Developed a reusable dropdown menu component with various functionalities.
    - Set up a Supabase client utility for handling authentication with a bearer token.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Button } from './ui/button'
+import { ModeToggle } from '@/components/ModeToggle';
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
 
 const Header = () => {
     return (
         <>
             <nav className='flex justify-between items-center px-10 py-3'>
                 <Link to="/" className='hover:scale-110 transition-all duration-200 cursor-pointer font-bold'>JobHunt</Link>
-                <Button variant="outline" className='cursor-pointer'>Login</Button>
+                <div className='flex gap-4 items-center'>
+                    <SignedOut>
+                        <SignInButton />
+                    </SignedOut>
+                    <SignedIn>
+                        <UserButton />
+                    </SignedIn>
+                    <ModeToggle />
+                </div>
             </nav>
         </>
     )
